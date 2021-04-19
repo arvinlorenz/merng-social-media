52e997b9837db3430cb9d3fb25ed52d91df3fb34
arvin lorenz <arvinlorenz@gmail.com>
2021-04-19 20:30:58 +0800

    folder structures - grapql folder, added nodemon

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
 const { ApolloServer } = require('apollo-server')
-const gql = require('graphql-tag')
 const mongoose = require('mongoose')
-
-const Post = require('./models/Post')
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
 const { MONGODB } = require('./config')
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`
-
-const resolvers = {
-  Query: {
-    getPosts: async () => {
-      try {
-        const posts = await Post.find()
-        return posts
-      } catch (e) {
-        throw new Error(e)
-      }
-    },
-  },
-}
 
 const server = new ApolloServer({
   typeDefs,
